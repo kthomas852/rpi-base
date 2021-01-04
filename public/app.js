e34e256892687100b58a88f687e4acf25adaf7d8
kthomas852 <kthomas852@yahoo.com>
2021-01-03 21:08:06 -0700

    delete

--- public/app.js
+++ public/app.js
@@ -3,21 +3,20 @@ function test() {
 }
 
 function redLight(){
-    // axios.get('/version')
-    axios.put('/api/pi/', {pin: 22});
-    setTimeout(()=>axios.delete('/api/pi/', {pin: 22}), 2000)
+    axios.put('/api/pi', {pin: 22});
+    setTimeout(()=>axios.delete('/api/pi', {pin: 22}), 2000)
 }
 
 function greenLight(){
-    axios.put('/api/pi/', {pin: 27});
-    setTimeout(()=>axios.delete('/api/pi/', {pin: 27}), 2000)
+    axios.put('/api/pi', {pin: 27});
+    setTimeout(()=>axios.delete('/api/pi', {pin: 27}), 2000)
 }
 
 function blueLight(){
-    axios.put('/api/pi/', {pin: 17});
-    setTimeout(()=>{axios.delete('/api/pi/', {pin: 17})}, 2000)
+    axios.put('/api/pi', {pin: 17});
+    setTimeout(()=>{axios.delete('/api/pi', {pin: 17})}, 2000)
 }
 
 function delay(){
-    setTimeout(()=>axios.put('/api/pi/', {pin: 22}), 2000)
+    setTimeout(()=>axios.delete('/api/pi', {pin: 22}), 2000)
 }
